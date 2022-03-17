1dd535fcaf55aa00b6d208592c3585f1f53622fb
luiggy macias <ferrinluiggy@gmail.com>
2022-03-17 10:51:12 -0500

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortfolioComponent },
+  { path: 'item', component: ItemComponent },
+  {path:'about', component:AboutComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
